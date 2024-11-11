478c438b29ab30d1eea19f51d6ebc185238c8a58
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-10 23:13:34 -0700

    Configuración para conexión del Frontend y el backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')  // Prefijo '/api' para todas las rutas en este controlador
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('endpoint')  // Ruta '/api/endpoint'
+  getEndpoint(): { message: string } {
+    return { message: 'Datos del backend recibidos correctamente' };
+  }
 
   @Get()
   getHello(): string {
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar CORS para permitir el acceso desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:3000', // Cambia esto al puerto donde corre tu frontend
+    credentials: true,
+  });
+
+  // Escuchar en el puerto especificado en la variable de entorno o en el 3001 por defecto
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
