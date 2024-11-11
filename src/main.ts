5570241bf963579e57ee0b4da4d50e196cc6b3f4
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-10 23:18:02 -0700

    Eliminar comentarios

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS para permitir el acceso desde el frontend
   app.enableCors({
-    origin: 'http://localhost:3000', // Cambia esto al puerto donde corre tu frontend
+    origin: 'http://localhost:3000',
     credentials: true,
   });
 
-  // Escuchar en el puerto especificado en la variable de entorno o en el 3001 por defecto
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
