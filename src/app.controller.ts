d63d9bec9f34d00aad32c88c6648d1de94483c13
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-11 20:23:33 -0700

    Conectar exitosamente la base de datos y el frontend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api')  // Prefijo '/api' para todas las rutas en este controlador
+@Controller('api') // Prefijo '/api' para todas las rutas en este controlador
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('endpoint')  // Ruta '/api/endpoint'
+  @Get('endpoint') // Ruta '/api/endpoint'
   getEndpoint(): { message: string } {
     return { message: 'Datos del backend recibidos correctamente' };
   }
