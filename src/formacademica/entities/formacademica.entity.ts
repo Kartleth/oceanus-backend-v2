3da6cdaf1f71eae2c679f9d5e1623a081e58cd77
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-12-27 18:09:35 -0700

    Combinación de backends para resolver conflictos

--- src/formacademica/entities/formacademica.entity.ts
+++ src/formacademica/entities/formacademica.entity.ts
@@ -12,9 +12,14 @@ export class Formacademica {
   @PrimaryGeneratedColumn()
   idacademicos: number;
 
-  @OneToOne(() => Persona, (persona) => persona.formacademica, {
-    onDelete: 'CASCADE',
-  }, (persona) => persona.datosAcademicos)
+  @OneToOne(
+    () => Persona,
+    (persona) => persona.formacademica,
+    {
+      onDelete: 'CASCADE',
+    },
+    (persona) => persona.datosAcademicos,
+  )
   empleado: Persona;
 
   @Column({ nullable: false })
