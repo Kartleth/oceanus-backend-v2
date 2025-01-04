137f154499ca78387651170632587df996d5243c
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-04 15:45:22 -0700

    Agregar carpeta auth con configuración

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsuariosService } from '../usuarios/usuarios.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsuariosService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(usuario: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByUsername(usuario); // Asegúrate de que estás usando el método correcto
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Compara la contraseña ingresada con la encriptada
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
