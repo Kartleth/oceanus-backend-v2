7b533616af85ff288c432f1e1f06f854c177b4de
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-09 18:29:50 -0700

    Agregar validaciones a CreateDocumentacionDto

--- src/documentacion/dto/create-documentacion.dto.ts
+++ src/documentacion/dto/create-documentacion.dto.ts
@@ -1,16 +1,59 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateDocumentacionDto {
+  @IsString()
+  @IsOptional()
   credencial: string;
+
+  @IsString()
+  @IsOptional()
   licencia: string;
+
+  @IsString()
+  @IsOptional()
   pasaporte: string;
+
+  @IsString()
+  @IsOptional()
   cv: string;
+
+  @IsString()
+  @IsOptional()
   curp: string;
+
+  @IsString()
+  @IsOptional()
   inss: string;
+
+  @IsString()
+  @IsOptional()
   constanciasat: string;
+
+  @IsString()
+  @IsOptional()
   foto: string;
+
+  @IsString()
+  @IsOptional()
   actnacimiento: string;
+
+  @IsString()
+  @IsOptional()
   estcuenta: string;
+
+  @IsString()
+  @IsOptional()
   altasegsocial: string;
+
+  @IsString()
+  @IsOptional()
   cedulaprofe: string;
+
+  @IsString()
+  @IsOptional()
   copiacontrato: string;
+
+  @IsString()
+  @IsOptional()
   comprodomicilio: string;
 }
