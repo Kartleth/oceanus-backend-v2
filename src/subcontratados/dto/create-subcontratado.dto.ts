46cdb2de87ff2e8182e77528ba79b73084c88e20
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-22 13:58:40 -0700

    Agregar decoradores a CreateSubcontratadoDto

--- src/subcontratados/dto/create-subcontratado.dto.ts
+++ src/subcontratados/dto/create-subcontratado.dto.ts
@@ -1,9 +1,26 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { EstadoSubcontratado } from '../entities/subcontratado.entity';
+
 export class CreateSubcontratadoDto {
+  @IsString()
   nombre: string;
+
+  @IsString()
   rfc: string;
+
+  @IsString()
   nss: string;
+
+  @IsString()
   ine: string;
+
+  @IsString()
   curp: string;
-  estado: string;
+
+  @IsEnum(EstadoSubcontratado)
+  estado: EstadoSubcontratado;
+
+  @IsOptional()
+  @IsString()
   doc: string;
 }
--- src/subcontratados/subcontratados.module.ts
+++ src/subcontratados/subcontratados.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SubcontratadosService } from './subcontratados.service';
 import { SubcontratadosController } from './subcontratados.controller';
+import { Subcontratado } from './entities/subcontratado.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subcontratado])],
   controllers: [SubcontratadosController],
   providers: [SubcontratadosService],
+  exports: [SubcontratadosService],
 })
 export class SubcontratadosModule {}
