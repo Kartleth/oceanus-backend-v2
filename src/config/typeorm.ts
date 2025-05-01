8b3824ec979a2fe66d5a978b1a7efce0cc885600
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-05-01 13:36:32 -0700

    CAMBIOS

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -13,7 +13,7 @@ const typeOrmConfig: DataSourceOptions = {
   port: parseInt(process.env.DATABASE_PORT, 10) || 5432, // Puerto de la base de datos
   username: process.env.DATABASE_USERNAME || 'postgres', // Usuario
   password: process.env.DATABASE_PASSWORD || 'ilseespinoza2708', // Contraseña
-  database: process.env.DATABASE_NAME || 'oceanusBD1', // Nombre de la base de datos
+  database: process.env.DATABASE_NAME || 'oceanusBD', // Nombre de la base de datos
   entities: ['dist/**/*.entity{.ts,.js}'], // Entidades que usa TypeORM
   migrations: ['dist/migrations/*{.ts,.js}'], // Migraciones
   // autoLoadEntities: true,  // Cargar automáticamente las entidades
